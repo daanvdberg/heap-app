408c2357766a3eb3f8e1013aebfa5ae7e18c39b5
Daan van den Berg <mail@daanvandenberg.com>
2023-02-04 16:35:38 +0100

    Remove use of userAgent on navigator for now

--- utils/index.ts
+++ utils/index.ts
@@ -28,12 +28,14 @@ export const isMobile = () => {
       hasTouchScreen = !!mQ.matches;
     } else if ('orientation' in window) {
       hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-      // Only as a last resort, fall back to user agent sniffing
-      const UA = typeof window !== 'undefined' ? navigator.userAgent : '';
-      hasTouchScreen =
-        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
     }
+    // else {
+    //   // Only as a last resort, fall back to user agent sniffing
+    //   const UA = navigator.userAgent;
+    //   hasTouchScreen =
+    //     /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+    //     /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    // }
   }
   return hasTouchScreen;
 };
